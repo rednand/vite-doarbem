6506028e6c1fce7de98834b3af40f236c44f58bf
rednand <nan-aguiar@hotmail.com>
2022-06-12 13:38:13 -0300

    ajuste pastas

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,18 +1,50 @@
 import React from "react";
 import "./styles.css";
-import svg from './arrow.svg';
+import wave from "./../../assets/imgs/Subtract.svg";
+import logo from "./../../assets/imgs/logo.svg";
+import face from "./../../assets/imgs/facebook.svg";
+import insta from "./../../assets/imgs/insta.svg";
+import linkedin from "./../../assets/imgs/linkedin.svg";
+import whats from "./../../assets/imgs/whatsapp.svg";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <img src={svg} alt="" />
-      <div className="footer-circle">
-        {/* <svg className="footer-svg" viewBox="0 0 500 150">
-          <path
-            d="M-50,80 C75,-57 99,80 502,107 L500,150 L-27,215 Z"
-            style={{ stroke: "none", fill: "#D75C5F" }}
-          ></path>
-        </svg> */}
+    <footer>
+      <div
+        className="footer__content"
+        style={{ backgroundImage: `url(${wave})` }}
+      >
+        <div className="footer__circle">
+          <img src={logo} alt="logo da Doar Bem" />
+        </div>
+        <div className="footer__upbottom">
+          <div className="footer__text">
+            <p> Lorem ipsum dolor sit amet</p>
+            <p> Lorem ipsum dolor sit amet</p>
+            <p> Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className="footer__redes">
+            <p>Venha nos conhecer!</p>
+            <div>
+              <a href="">
+                <img src={linkedin} alt="icone do linkedin" width={56} />
+              </a>
+              <a href="">
+                <img src={face} alt="icone do facebook" width={56} />
+              </a>
+              <a href="">
+                <img src={insta} alt="icone do instagram" width={52} />
+              </a>
+              <a href="">
+                <img src={whats} alt="icone do whatsapp" width={52} />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="footer__bottom">
+          Somos uma plataforma que conecta ongs e pessoas que querem contribuir
+          para uma sociedade melhor.
+        </div>
       </div>
     </footer>
   );
